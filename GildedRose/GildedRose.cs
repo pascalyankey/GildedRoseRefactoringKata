0bc66c5288ca31620788cb11dec063e907f8c131
Pascal Yankey <pascalyankey@gmail.com>
2024-10-26 12:08:04 +0200

    Add project files.

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata;
+
+public class GildedRose
+{
+    IList<Item> Items;
+
+    public GildedRose(IList<Item> Items)
+    {
+        this.Items = Items;
+    }
+
+    public void UpdateQuality()
+    {
+        const int maxQuality = 50;
+        const int minQuality = 0;
+
+        foreach (var item in Items) 
+        {
+            var isSulfuras = item.Name == "Sulfuras, Hand of Ragnaros";
+            var isAgedBrie = item.Name == "Aged Brie";
+            var isBackstagePasses = item.Name == "Backstage passes to a TAFKAL80ETC concert";
+
+            if (!isSulfuras)
+            {
+                item.SellIn--;
+
+                var isMaxQuality = item.Quality == maxQuality;
+                var isMinQuality = item.Quality == minQuality;
+                var isPassedSellDate = item.SellIn < 0;
+                var isTenDaysSell = item.SellIn < 10 && item.SellIn > 4;
+                var isFiveDaysSell = item.SellIn < 5;
+                var exceedsTenDaysSellQualityLimit = item.Quality + 2 > 50;
+                var exceedsFiveDaysSellQualityLimit = item.Quality + 3 > 50;
+                var exceedsPassedBySellDateQualityMin = isPassedSellDate && item.Quality - 2 < minQuality;
+                var exceedsPassedBySellDateQualityMax = isPassedSellDate && item.Quality + 2 > maxQuality;
+
+                if (isAgedBrie) // Case Aged Brie
+                {
+                    if (!isMaxQuality)
+                    {
+                        if (isPassedSellDate)
+                        {
+                            if (exceedsPassedBySellDateQualityMax)
+                            {
+                                item.Quality = maxQuality;
+                            } 
+                            else
+                            {
+                                item.Quality += 2;
+                            }
+                        }
+                        else
+                        {
+                            item.Quality++;
+                        }
+                    }
+                }
+                else if (isBackstagePasses) // Case Backstage passes to a TAFKAL80ETC concert
+                {
+                    if (isPassedSellDate)
+                    {
+                        item.Quality = 0;
+                    }
+                    else
+                    {
+                        if (!isMaxQuality)
+                        {
+                            if (isTenDaysSell)
+                            {
+                                if (exceedsTenDaysSellQualityLimit)
+                                {
+                                    item.Quality = maxQuality;
+                                } 
+                                else
+                                {
+                                    item.Quality += 2;
+                                }
+                            }
+                            else if (isFiveDaysSell)
+                            {
+                                if (exceedsFiveDaysSellQualityLimit)
+                                {
+                                    item.Quality = maxQuality;
+                                }
+                                else
+                                {
+                                    item.Quality += 3;
+                                }
+                            }
+                            else
+                            {
+                                item.Quality++;
+                            }
+                        }
+                    }
+                }
+                else // Case normal items
+                {
+                    if (!isMinQuality)
+                    {
+                        if (isPassedSellDate)
+                        {
+                            if (exceedsPassedBySellDateQualityMin)
+                            {
+                                item.Quality = minQuality;
+                            }
+                            else
+                            {
+                                item.Quality -= 2;
+                            }
+                        } 
+                        else
+                        {
+                            item.Quality--;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,107 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("foo", Items[0].Name);
+    }
+
+    [Fact]
+    public void UpdateQuality_ItemNameContainingSulfuras_ShouldNotBeAltered()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(80, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_NormalItemWithPassedSellByDate_DegradesTwice()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(3, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_Item_ShouldNeverBeNegative()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_AgedBrieItem_Increases()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 20 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(21, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_AgedBrieItemWithPassedSellByDate_IncreasesTwice()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = -1, Quality = 20 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(22, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_AgedBrieItem_IsNeverMoreThanFifty()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 15, Quality = 50 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(50, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_BackstagePassesItemSellInTenDays_IncreasesByTwo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 40 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(42, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_BackstagePassesItemSellInFiveDays_IncreasesByThree()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 40 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(43, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_BackstagePassesItemWithPassedSellByDate_DropsToZero()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 40 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_ConjuredItemWithPassedSellByDate_DegradesTwice()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = -1, Quality = 30 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(28, Items[0].Quality);
+    }
+}
